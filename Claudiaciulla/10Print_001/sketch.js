970b8be3e448153fe5cd3cbe6734f2ae34b2350c
ClaudiaC <129759071+Claudiaciulla@users.noreply.github.com>
2023-06-21 22:00:14 +0200

    10_Print_add

---
+++ Claudiaciulla/10Print_001/sketch.js
@@ -0,0 +1,55 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+
+// 10PRINT
+// https://www.youtube.com/watch?v=bEyTZ5ZZxZs
+
+let x = 0;
+let y = 0;
+let d = 50;
+strokeSize=5;
+
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  background(0);
+ 
+}
+
+function draw() {
+   stroke(255);
+   strokeWeight(strokeSize);
+   if(random(1) < 0.4){
+      point(x+d/2,y+d);
+    }
+    else{
+      
+      line(x + strokeSize,y + d,x + d - strokeSize,y + d);
+    }
+  x += d;
+  if (x > width) {
+    x = 0;
+    y +=  d;
+  }
+ 
+  
+  if (y > height) {
+    background(0);
+    strokeSize=strokeSize-2;
+    d=d/2;
+    x=0;
+    y = 0;
+  }
+
+}
+
+function mousePressed() {
+  background(0);
+    strokeSize=strokeSize+2;
+    d=d*2;
+    x=0;
+    y = 0;
+    }
+  
+
